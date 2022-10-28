5686350178e5fb3be99921c7aaeb911f79c26077
jordnkr <jordnkr408@gmail.com>
2022-10-28 10:20:05 -0500

    updates

--- src/hooks/use-message.js
+++ src/hooks/use-message.js
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const SERVER_URL = "https://chat-server-jk3lh509uq2j34qag4.herokuapp.com";
+const SERVER_URL = "http://localhost:4000";
 
 const useMessage = (room) => {
   const [chatMessages, setChatMessages] = useState([]);
+  const [userId, setUserId] = useState(Math.floor(Math.random() * Date.now()));
   let socketRef = useRef();
 
   useEffect(() => {
@@ -14,19 +15,24 @@ const useMessage = (room) => {
       },
     });
 
+    socketRef.current.emit("new-message", [userId, `Anonymous${userId} joined the chat`]);
+
     // receive a message from the server
-    socketRef.current.on("mail delivery", (message) => {
-      setChatMessages((prevMessages) => [...prevMessages, message]);
+    socketRef.current.on("mail-delivery", (message) => {
+      if (message[0] !== userId) {
+        setChatMessages((prevMessages) => [...prevMessages, message[1]])
+      }
     });
 
     return () => {
+      socketRef.current.emit("new-message", [userId, `Anonymous${userId} left the chat`]);
       socketRef.current.disconnect();
     };
-  }, [room]);
+  }, [room, userId]);
 
   const sendMessage = (message) => {
     // send a message to the server
-    socketRef.current.emit("new message", message);
+    socketRef.current.emit("new-message", message);
   };
 
   return {
