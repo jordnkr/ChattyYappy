0a17d78d06c806a87b46f0fde1efd8f9af1e60d5
jordnkr <jordnkr408@gmail.com>
2022-10-30 01:45:06 -0500

    Adding user count

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,13 +3,16 @@ import classes from "./Users.module.css";
 
 const Users = (props) => {
   return (
-    <div>
-      {props.users.map((user, i) => (
-        <p key={i} className={classes.user}>
-          {user}
-        </p>
-      ))}
-    </div>
+    <>
+      <p>Users in room: {props.users.length}</p>
+      <div>
+        {props.users.map((user, i) => (
+          <p key={i} className={classes.user}>
+            {user}
+          </p>
+        ))}
+      </div>
+    </>
   );
 };
 
