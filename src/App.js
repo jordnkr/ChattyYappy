1182e665f813d2a9ba9455dfb3ac3b5705c0e7df
jordnkr <jordnkr408@gmail.com>
2022-10-30 01:35:39 -0500

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Container from "./components/UI/Container";
 import Title from "./components/UI/Title";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/UI/Footer";
 
 function App() {
   return (
-    <Container>
-      <Title />
-      <Outlet />
-    </Container>
+    <>
+      <Container>
+        <Title />
+        <Outlet />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/UI/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Footer.module.css";
+
+const Footer = (props) => {
+  return (
+    <div className={classes.footer}>
+      <p>&copy;<a target="_blank" rel="noreferrer" href="https://github.com/jordnkr">jordnkr</a></p>
+    </div>
+  );
+};
+
+export default Footer;
