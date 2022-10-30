a022320a794746e988dfb464edb9db028ae543ce
jordnkr <jordnkr408@gmail.com>
2022-10-30 10:51:02 -0500

    removing container, not currently needed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import Container from "./components/UI/Container";
 import Title from "./components/UI/Title";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/UI/Footer";
 
 function App() {
   return (
-    <Container>
+    <>
       <Title />
       <Outlet />
       <Footer />
-    </Container>
+    </>
   );
 }
 
