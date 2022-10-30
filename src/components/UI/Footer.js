1b6e43a6296df79f380c92a6eac588e6bfbf75ab
jordnkr <jordnkr408@gmail.com>
2022-10-30 10:20:47 -0500

    some CSS cleanup

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -4,7 +4,12 @@ import classes from "./Footer.module.css";
 const Footer = (props) => {
   return (
     <div className={classes.footer}>
-      <p>&copy;<a target="_blank" rel="noreferrer" href="https://github.com/jordnkr">jordnkr</a></p>
+      <p>
+        &copy;{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/jordnkr">
+          jordnkr
+        </a>
+      </p>
     </div>
   );
 };
