4c938d3ad3f9bbc16608e47d5ec4a4b21724052e
jordnkr <jordnkr408@gmail.com>
2022-11-02 20:52:17 -0500

    message style changes

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,17 +3,31 @@ import classes from "./Message.module.css";
 
 const Message = (props) => {
   return (
-    <div
-      className={`${classes.message} ${
-        props.messageId === props.myId
-          ? classes["my-message"]
-          : classes["other-message"]
-      }`}
-    >
-      {props.network && `${props.user} ${props.content}`}
-      {!props.network && <p className={classes["message-user"]}><b>{props.user}</b> ({props.timestamp})</p>}
-      {!props.network && <p className={classes["message-text"]}>{props.content}</p>}
-    </div>
+    <>
+      {!props.network && (
+        <p
+          className={`${classes["message-user"]} ${
+            props.messageId === props.myId
+              ? classes["my-message-user"]
+              : classes["other-message-user"]
+          }`}
+        >
+          <b>{props.user}</b> ({props.timestamp})
+        </p>
+      )}
+      <div
+        className={`${classes.message} ${
+          props.messageId === props.myId
+            ? classes["my-message"]
+            : classes["other-message"]
+        }`}
+      >
+        {props.network && `${props.user} ${props.content}`}
+        {!props.network && (
+          <p className={classes["message-text"]}>{props.content}</p>
+        )}
+      </div>
+    </>
   );
 };
 
